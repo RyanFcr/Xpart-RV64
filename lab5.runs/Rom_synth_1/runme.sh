77e7c7b30c04f62b4a54ba4770222685943b02c0
Gax-c <2388231068@qq.com>
2023-07-03 19:11:19 +0800

    modify immgen

--- lab5.runs/Rom_synth_1/runme.sh
+++ lab5.runs/Rom_synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='F:/Vivado labs/sys3_lab1_myversion/lab5.runs/Rom_synth_1'
+HD_PWD='F:/Vivado labs/Xpart-RV64/lab5.runs/Rom_synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- lab5.srcs/sources_1/new/immgen.v
+++ lab5.srcs/sources_1/new/immgen.v
@@ -14,13 +14,13 @@ module immgen(
     assign U_type = (inst[6:0] == 7'b0110111) || (inst[6:0] == 7'b0010111);
     assign CSR_type = (inst[6:0] == 7'b1110011 && (inst[14:12] == 3'b001 || inst[14:12] == 3'b010 || inst[14:12] == 3'b011 || inst[14:12] == 3'b101 || inst[14:12] == 3'b110 || inst[14:12] == 3'b111));
         
-    wire [31:0] I_imm, S_imm, B_imm, J_imm, U_imm, CSR_imm; 
-    assign I_imm = {{20{inst[31]}}, inst[31:20]};
-    assign S_imm = {{20{inst[31]}}, inst[31:25], inst[11:7]};
-    assign B_imm = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8],1'b0};
-    assign J_imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21],1'b0};
-    assign U_imm = {inst[31:12], 12'b0};
-    assign CSR_imm = {27'b0, inst[19:15]}; 
+    wire [63:0] I_imm, S_imm, B_imm, J_imm, U_imm, CSR_imm; 
+    assign I_imm = {{52{inst[31]}}, inst[31:20]};
+    assign S_imm = {{52{inst[31]}}, inst[31:25], inst[11:7]};
+    assign B_imm = {{52{inst[31]}}, inst[7], inst[30:25], inst[11:8],1'b0};
+    assign J_imm = {{52{inst[31]}}, inst[19:12], inst[20], inst[30:21],1'b0};
+    assign U_imm = {32'b0, inst[31:12], 12'b0};
+    assign CSR_imm = {59'b0, inst[19:15]}; 
     assign imm = I_type ? I_imm : S_type ? S_imm : B_type ? B_imm : J_type ? J_imm : U_type ? U_imm : CSR_type ? CSR_imm : 32'b0;
     
 endmodule
